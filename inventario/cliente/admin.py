adae76994dc948a7b66ce040dbfb5fc65dcc8837
David <david.irusta@itscordoba.edu.ar>
2025-10-31 19:55:47 -0300

    No conservar

--- inventario/cliente/admin.py
+++ inventario/cliente/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cliente
 
-# Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'numero_documento', 'email', 'telefono', 'direccion']
+    list_filter = ['email']
+    search_fields = ['nombre', 'email', 'numero_documento']
--- inventario/ventas/admin.py
+++ inventario/ventas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Venta
 
 # Register your models here.
+@admin.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'fecha', 'total']
+    list_filter = ['fecha']
+    search_fields = ['cliente_nombre', 'cliente_email']
