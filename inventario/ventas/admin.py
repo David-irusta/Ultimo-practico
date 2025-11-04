46377b0c62d856e63fcbc70fb377dab41f2f08cd
David <david.irusta@itscordoba.edu.ar>
2025-11-04 11:55:45 -0300

    Resolviendo

--- inventario/ventas/admin.py
+++ inventario/ventas/admin.py
@@ -4,6 +4,6 @@ from .models import Venta
 # Register your models here.
 @admin.register(Venta)
 class VentaAdmin(admin.ModelAdmin):
-    list_display = ['id', 'cliente', 'fecha', 'total']
+    list_display = ['codigo', 'cliente', 'fecha', 'total']
     list_filter = ['fecha']
     search_fields = ['cliente_nombre', 'cliente_email']
